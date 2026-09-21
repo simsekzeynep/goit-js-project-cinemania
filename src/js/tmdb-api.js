2577ee58d36529095dc2f70699495fbf1ddf988d
Zeynep Simsek <talas0038@hotmail.com>
2026-09-22 01:56:58 +0300

    feat: add TMDB genres API

---
+++ src/js/tmdb-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+// API İstekleri için Temel Sabitler
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+//TMDB API'den film türlerini getirir.
+export async function getGenres() {
+    const response = await axios.get(`${BASE_URL}/genre/movie/list`, {
+    params: {
+        api_key: API_KEY,
+        language: 'en',
+    },
+    });
+    return response.data.genres;
+}
